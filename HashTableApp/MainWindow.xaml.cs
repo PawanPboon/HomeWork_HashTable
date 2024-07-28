864fd88368e1fac4bcc5c5c75e8d3901466be479
Pawan_BU <149138396+PawanPboon@users.noreply.github.com>
2024-07-28 16:23:45 +0700

    Commit
    Homework Hashtable
    1. Added Key
    2. Remove Key
    3. Show  Value
    4. Show  Count data

---
+++ HashTableApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashTableApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Add
+            MessageBox.Show("Added Complete");
+            hashtable.Add(txtKey.Text,txtValue.Text);
+           
+        }
+
+        private void removeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //remove
+            MessageBox.Show("Removed Complete");
+            hashtable.Remove(txtKey.Text);
+        }
+
+        private void showBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            ICollection icollection = hashtable.Keys;
+            //show
+            foreach(string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+                
+            }
+            MessageBox.Show("Count is       " + hashtable.Count.ToString());
+        }
+    }
+}
